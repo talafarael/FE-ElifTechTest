3bba1e45f4787c99346e1247b24fc640b22f317f
artem <artemk2504@gmail.com>
2025-03-23 18:16:04 +0200

    feat:start adn config

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className="bg-custom-ligh h-[100px] flex items-center w-[100vw] ">
+      <h1 className='text-[20px]'>Title</h1>
+
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'custom-light': '#F8F3D9',
+        'custom-beige': '#EBE5C2',
+        'custom-grey': '#B9B28A',
+        'custom-dark': '#504B38',
+      },
+    },
+  },
+}
